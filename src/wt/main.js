21f1bbc772208f25c7ef3098f7739f9c5f7f7d40
casder <casderiopus@gmail.com>
2022-12-05 21:26:00 +0300

    feat: complete worker threads module

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,35 @@
+import { Worker } from 'worker_threads';
+import { cpus } from 'os';
+
 const performCalculations = async () => {
-    // Write your code here
+  let number = 10;
+
+  const results = (await Promise.allSettled(cpus().map(() => {
+    return new Promise((resolve, reject) => {
+      const worker = new Worker('./worker.js', {
+        workerData: number++,
+      });
+
+      worker.on('message', (message) => {
+        typeof message === 'number' ?
+          resolve({status: 'resolved', data: message}) : reject({status: 'error', data: null});
+      });
+
+      worker.on('error', () => {
+        reject({status: 'error', data: null});
+      });
+
+      worker.on('exit', (code) => {
+        if (code !== 0) {
+          reject({status: 'error', data: null});
+        }
+      });
+    });
+  }))).map((promise) => {
+    return promise.status === 'fulfilled' ? promise.value : promise.reason;
+  });
+
+  console.log(results);
 };
 
 await performCalculations();
