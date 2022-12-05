efe4b4384094e5a1ea1281e52694e702b18bed42
casder <casderiopus@gmail.com>
2022-12-05 21:03:21 +0300

    feat: complete zip module

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { createGzip } from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const filesPath = path.join(__dirname, 'files');
+
+  const gzip = createGzip();
+
+  const readStream = fs.createReadStream(path.join(filesPath, 'fileToCompress.txt'));
+  const writeStream = fs.createWriteStream(path.join(filesPath, 'archive.gz'));
+
+  readStream.pipe(gzip).pipe(writeStream);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { createGunzip } from 'zlib';
+import fs from 'fs';
+
 const decompress = async () => {
-    // Write your code here 
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const filesPath = path.join(__dirname, 'files');
+
+  const unzip = createGunzip();
+
+  const readStream = fs.createReadStream(path.join(filesPath, 'archive.gz'));
+  const writeStream = fs.createWriteStream(path.join(filesPath, 'fileToCompress.txt'));
+
+  readStream.pipe(unzip).pipe(writeStream);
 };
 
 await decompress();
