7cd3b6e0e3aa1367ac57af0bd74c8db7b6ead5cf
casder <casderiopus@gmail.com>
2022-12-05 20:25:02 +0300

    feat: complete hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import crypto from 'crypto';
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const filePath = path.join(__dirname, 'files/fileToCalculateHashFor.txt');
+
+  const buffer = await fs.readFile(filePath);
+  const hashSum = crypto.createHash('sha256').update(buffer);
+
+  console.log(hashSum.digest('hex'));
 };
 
 await calculateHash();
